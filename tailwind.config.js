85ec88e225f6cb25ce409e3fab080d28aab058e8
GrommuL <miketsvg@gmail.com>
2023-07-14 10:35:07 +0600

    feat: added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,21 @@ export default {
 			container: {
 				padding: '15px',
 				center: true
+			},
+			colors: {
+				textPrimary: '#2C2D2E',
+				textSecondary: '#919399',
+				primary: '#465EFD',
+				bgPrimary: '#FBFBFB',
+				bgTertiary: '#F0F0F0',
+				bgSecondary: '#EDEFFF',
+				borderColor: 'rgba(0, 16, 61, 0.12))',
+				error: '#ED0A34',
+				success: '#0DC268',
+				link: '#005BD1',
+				overlay: 'rgba(0, 16, 61, 0.48)',
+				white: '#FFFFFF',
+				black: '#000000'
 			}
 		}
 	},
