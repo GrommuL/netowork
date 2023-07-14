61d67dda73e1d12d3c3550745281580744a715c2
GrommuL <miketsvg@gmail.com>
2023-07-14 14:13:55 +0600

    fix: added container and header styles

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,17 +1,20 @@
 import { Button } from '@/shared/ui'
 import { HeaderLinkButton, HeaderLogoLinkButton } from './ui'
+import { Container } from '@/shared/ui/Container'
 
 export const Header = () => {
 	return (
-		<header className='container flex items-center justify-between h-[80px]'>
-			<div className='flex items-center gap-[48px]'>
-				<HeaderLogoLinkButton />
-				<div className='flex items-center'>
-					<HeaderLinkButton label='Создать задание' href='/' />
-					<HeaderLinkButton label='Найти задание' href='/' />
+		<header className='shadow-header'>
+			<Container className='flex items-center justify-between h-[80px] '>
+				<div className='flex items-center gap-[48px]'>
+					<HeaderLogoLinkButton />
+					<div className='flex items-center'>
+						<HeaderLinkButton label='Создать задание' href='/' />
+						<HeaderLinkButton label='Найти задание' href='/' />
+					</div>
 				</div>
-			</div>
-			<Button label='Вход и регистрация' />
+				<Button label='Вход и регистрация' />
+			</Container>
 		</header>
 	)
 }
